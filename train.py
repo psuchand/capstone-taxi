b6293949f04c7eea2caa66324dab56742a76d646
Suchandan <psuchand@umich.edu>
2016-10-05 17:57:10 -0700

    cleanup training routine

--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 from ml_helper_functions import *
+from helper_functions import *
+from pre_process import *
+
+import pandas as pd
 
 DATA_DIR = "./"
 TRAINING_DIR = DATA_DIR + "training/"
 
 if __name__ == "__main__":
-    from helper_functions import *
-    from pre_process import *
+	print "This function trains the ML model based on rides.csv. Output is in " + TRAINING_DIR + "\n\n"
     print "Reading data...",
     rides = pd.read_csv(DATA_DIR + "rides.csv")
     print "done."
